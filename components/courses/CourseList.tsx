b02139857ca0b2ecd5efa6f00f8304e922cf6118
Freilin Manzano <freilin.manzano@externos.bci.cl>
2022-07-11 21:50:28 -0400

    add link to certificates.

--- components/courses/CourseList.tsx
+++ components/courses/CourseList.tsx
@@ -1,48 +1,58 @@
 import React, { FC } from 'react';
-import { Grid, Image } from "@nextui-org/react";
+import { Grid, Image, Link } from "@nextui-org/react";
+import { ICourse } from '../../interfaces/course';
 
 const CourseList: FC = (): any => {
     return (
         courses.map((course, i) => (
             <Grid key={i} sm={5}>
-                <Image
-                    objectFit='fill'
-                    autoResize={true}
-                    width={310}
-                    height={220}
-                    src={course.path}
-                    alt={course.name}
-                />
-            </Grid>
+                <Link href={course.url} target={'_blank'} >
+                    <Image
+                        objectFit='fill'
+                        autoResize={true}
+                        width={310}
+                        height={220}
+                        src={course.pathImage}
+                        alt={course.name}
+                    />
+                </Link>
+            </Grid >
         ))
     )
 }
 
 export default CourseList;
 
-const courses: Array<any> = [
+
+const courses: Array<ICourse> = [
     {
         name: 'next-frmwk-prod',
-        path: '/images/courses/next-frmwk-prod.png'
+        pathImage: '/images/courses/next-frmwk-prod.png',
+        url: 'https://udemy-certificate.s3.amazonaws.com/pdf/UC-625fca48-56d0-499a-abd0-2e3299aeb3a0.pdf'
     },
     {
         name: 'next-react-guide',
-        path: '/images/courses/next-react-guide.png'
+        pathImage: '/images/courses/next-react-guide.png',
+        url: 'https://udemy-certificate.s3.amazonaws.com/pdf/UC-ea89ede5-8ad5-4182-bc23-d467a82f7ab4.pdf'
     },
     {
         name: 'web-bootcamp2022',
-        path: '/images/courses/web-bootcamp2022.png'
+        pathImage: '/images/courses/web-bootcamp2022.png',
+        url: 'https://udemy-certificate.s3.amazonaws.com/pdf/UC-fa37664f-a127-40f3-b590-64893ff75dee.pdf'
     },
     {
         name: 'master-cqrs',
-        path: '/images/courses/master-cqrs.png'
+        pathImage: '/images/courses/master-cqrs.png',
+        url: 'https://udemy-certificate.s3.amazonaws.com/pdf/UC-2486bc7f-f452-4c96-8f0d-9c9e229a7223.pdf'
     },
     {
         name: 'flutter-complete-guide',
-        path: '/images/courses/flutter-complete-guide.png'
+        pathImage: '/images/courses/flutter-complete-guide.png',
+        url: 'https://udemy-certificate.s3.amazonaws.com/pdf/UC-5b81bda1-1845-4530-92df-681e05705f46.pdf'
     },
     {
         name: 'flutter-2021',
-        path: '/images/courses/flutter-2021.png'
+        pathImage: '/images/courses/flutter-2021.png',
+        url: 'https://udemy-certificate.s3.amazonaws.com/pdf/UC-07aabcbd-8c1e-461d-9518-e69cbf0d998c.pdf'
     }
 ];
